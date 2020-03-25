5eac83590c0c80a65240c1643aee17df04dec8b0
Richard <phalarichard1@gmail.com>
2020-03-25 23:22:11 +0200

    fixing errors

--- scraper_web/my_scraper.rb
+++ scraper_web/my_scraper.rb
@@ -1,43 +1,42 @@
-require "httparty"
-require "nokogiri"
+require 'httparty'
+require 'nokogiri'
 
 class My_scraper
   attr_reader :parsing_page
 
   def initialize
-    url = HTTParty.get("http://quotes.toscrape.com/")
+    url = HTTParty.get('http://quotes.toscrape.com/')
     @parsing_page = Nokogiri::HTML(url)
   end
 
   def famous_quotes
-    quote_list = parsing_page.css("div.quote") 
+    quote_list = parsing_page.css('div.quote')
   end
 
   def my_quote
-    quote = famous_quotes.css("span.text").children.map {|quoted| quoted.text}.compact
+    quote = famous_quotes.css('span.text').children.map(&:text).compact
   end
 
   def my_author
-    author = famous_quotes.css("span").css("small.author").children.map {|writer| writer.text}.compact
+    author = famous_quotes.css('span').css('small.author').children.map(&:text).compact
   end
 
   def my_tags
-    anchor_tags = famous_quotes.css("div.tags").css("a.tag").children.map {|taged| taged.text}.compact
+    anchor_tags = famous_quotes.css('div.tags').css('a.tag').children.map(&:text).compact
   end
 
   the_scraper = My_scraper.new
   quotes = the_scraper.my_quote
   authors = the_scraper.my_author
   tags = the_scraper.my_tags
-  
 
   (0...quotes.size).each do |i|
-    puts "-------Quote: #{i+1}---------"
+    puts "-------Quote: #{i + 1}---------"
     puts "\n"
     puts "QUOTE: #{quotes[i]}"
-    puts ""
+    puts ''
     puts "AUTHOR: #{authors[i]}"
-    puts ""
+    puts ''
     puts "TAGS: #{tags[i]}"
     puts "\n"
   end
