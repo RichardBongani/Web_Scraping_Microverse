467763267e0effd8f902fac219fe218e4aa8660c
Richard <phalarichard1@gmail.com>
2020-04-14 20:40:29 +0200

    fixing errors

--- bin/csv.rb
+++ bin/csv.rb
@@ -3,7 +3,7 @@ require 'csv'
 
 class MyCsvFile < ScraperVars
   def csv_file
-    CSV.foreach('my_quotes.csv', 'r') do |csv| 
+    CSV.foreach('my_quotes.csv', 'r') do |csv|
       puts csv.inspect
     end
   end
--- bin/scraper.rb
+++ bin/scraper.rb
@@ -5,6 +5,6 @@ class MyScraper
   attr_reader :parsing_url
   def initialize
     url = RestClient.get('http://quotes.toscrape.com/')
-    @parsing_url = Nokogiri::HTML(url) 
+    @parsing_url = Nokogiri::HTML(url)
   end
 end
