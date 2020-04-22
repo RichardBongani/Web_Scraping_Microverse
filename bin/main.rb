08c9769e3d8040b432592ed618da2e0d03cd6a86
Richard <phalarichard1@gmail.com>
2020-04-23 00:00:08 +0200

    fixing errors and rearanging my files

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,8 @@
-require_relative 'csv.rb'
-require_relative 'scraper_vars.rb'
-require_relative 'scraper.rb'
+#!/usr/bin/env ruby
+
+require_relative '../lib/csv.rb'
+require_relative '../lib/scraper_vars.rb'
+require_relative '../lib/scraper.rb'
 
 test = MyCsvFile.new
 puts test.csv_file
--- lib/csv.rb
+++ lib/csv.rb
@@ -1,9 +1,10 @@
 require_relative 'scraper_vars.rb'
+require_relative 'scraper.rb'
 require 'csv'
 
-class MyCsvFile < ScraperVars
+class MyCsvFile < MyScraper
   def csv_file
-    CSV.foreach('my_quotes.csv', 'r') do |csv|
+    CSV.foreach('my_quotes.csv') do |csv|
       puts csv.inspect
     end
   end
